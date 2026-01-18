deef3dfc87cea9340a6f695ff6a71f3a215f5474
istxing <xlqb@foxmail.com>
2026-01-18 20:09:31 +0800

    11

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { ClipCardComponent } from './ClipCard';
-export { SearchBar } from './SearchBar';
-export { EmptyState } from './EmptyState';
--- src/hooks/index.ts
+++
@@ -1,2 +0,0 @@
-export { useChromeMessaging } from './useChromeMessaging';
-export { useI18n } from './useI18n';
